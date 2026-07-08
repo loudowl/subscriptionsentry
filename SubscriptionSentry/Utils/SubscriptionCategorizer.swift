37032eb78e34fa773598e82e7dcbcbc7bdc24670
Chris <loudowl@mac.com>
2026-07-08 08:55:20 -0700

    feat: initial implementation

---
+++ SubscriptionSentry/Utils/SubscriptionCategorizer.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+class SubscriptionCategorizer {
+    static func categorizeSubscriptions(texts: [String]) -> [Subscription] {
+        // Example categorization logic
+        // This function should parse texts and create Subscription objects
+        return []
+    }
+}
