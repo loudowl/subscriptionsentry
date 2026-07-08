8c9cfc5cf07e02cf89ac3d5f45507ee163a2c0c3
Chris <loudowl@mac.com>
2026-07-08 08:55:21 -0700

    feat: initial implementation

---
+++ SubscriptionSentry/Utils/ROIAnalyzer.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class ROIAnalyzer {
+    static func calculateROI(for subscriptions: [Subscription]) -> Double {
+        // Example ROI calculation logic
+        return 0.0
+    }
+}
