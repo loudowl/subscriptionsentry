5c29ac04e6145dedd394bcb1ab4d5273bae9356f
Chris <loudowl@mac.com>
2026-07-08 08:55:23 -0700

    feat: initial implementation

---
+++ SubscriptionSentry/Tests/SubscriptionSentryTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import SubscriptionSentry
+
+class SubscriptionSentryTests: XCTestCase {
+
+    func testExample() throws {
+        // Example test case
+        XCTAssertTrue(true)
+    }
+}
